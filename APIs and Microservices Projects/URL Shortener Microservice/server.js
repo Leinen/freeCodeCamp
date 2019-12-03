7dfb169a2d1aa96e488517e9f74daec97c5a4b91
Leinen <36449763+Leinen@users.noreply.github.com>
2019-12-04 00:44:17 +0100

    Update server.js
    solved :)

--- APIs and Microservices Projects/URL Shortener Microservice/server.js
+++ APIs and Microservices Projects/URL Shortener Microservice/server.js
@@ -26,10 +26,41 @@ app.get('/', function(req, res){
 });
 
   
+//Mount the body-parser middleware  here
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}));
+// url exists
+// use await and async - https://stackoverflow.com/questions/26007187/node-js-check-if-a-remote-url-exists
+const util = require('util');
+const urlExists = util.promisify(require('url-exists'));
+
+// url container
+let urls = [];
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.route("/api/shorturl/new").post(async (req, res) => {
+  let url = req.body.url;
+  let isExists = await urlExists(url);  
+  if (!isExists) return res.send({"error":"invalid URL"});  
+  
+  // shorten url and send response
+  let index = urls.findIndex(item => item.original_url === url);
+  if(index !== -1) return res.send(urls[index]);
+  let obj = {original_url: url, short_url: urls.length}
+  urls.push(obj);
+  res.send(obj);  
+  console.log(urls)
 });
+
+// shorturl endpoint
+app.route("/api/shorturl/:short").get((req, res) => {
+  let short_url = parseInt(req.params.short)
+  let index = urls.findIndex(item => item.short_url === short_url);
+  if (index === -1) return res.send({"error":"invalid short-URL"});
+  res.redirect(urls[index].original_url);
+  // console.log(urls[index].original_url)
+  // console.log(index)  
+})
 
 
 app.listen(port, function () {
