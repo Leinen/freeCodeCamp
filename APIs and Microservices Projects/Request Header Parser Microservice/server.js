e9587196a069e1b5f1c7f2976b5740532029b7ed
Leinen <36449763+Leinen@users.noreply.github.com>
2019-12-03 20:50:47 +0100

    Update server.js
    solved :)

--- APIs and Microservices Projects/Request Header Parser Microservice/server.js
+++ APIs and Microservices Projects/Request Header Parser Microservice/server.js
@@ -20,8 +20,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/whoami', function (req, res) {
+  let ip = res.connection.remoteAddress;
+  let language = req.headers['accept-language'];
+  let software = req.headers['user-agent'];
+  res.json({
+    "ipaddress": ip,
+    "language": language,
+    "software": software,
+  });
 });
 
 
