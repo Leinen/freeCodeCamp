a227653d60a29f999f9180c645894deb7fdbd773
Leinen <36449763+Leinen@users.noreply.github.com>
2019-12-04 18:27:46 +0100

    Update server.js
    update: now data persistence

--- APIs and Microservices Projects/Exercise Tracker/server.js
+++ APIs and Microservices Projects/Exercise Tracker/server.js
@@ -22,14 +22,7 @@ app.get('/', (req, res) => {
 
 // API end points here
 
-const users = [];
-const exercises = [];
-const getUserIndex = (username) => { // username or id
-  let ret;
-  ret = users.findIndex(item => item.username === username);
-  if (ret === -1) ret = users.findIndex(item => item._id === username);
-  return ret;
-}
+// helper functions
 const checkIsDate = (date) => date.toUTCString() !== "Invalid Date";
 const formatDate = (date) => { // modified from: https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-dd
     let d = date,
@@ -45,29 +38,85 @@ const formatDate = (date) => { // modified from: https://stackoverflow.com/quest
     return [year, month, day].join('-');
 }
 
+// User (class)
+const fs = require('fs');
+class User {
+  constructor () {
+    this.PATH_USERS = 'data/users.json';
+    this.PATH_EXERCISES = 'data/exercises.json';
+    this.users = this._readFileUsers(); 
+    this.exercises = this._readFileExercises();
+  }
+  _readFileUsers ()     { return (fs.existsSync(this.PATH_USERS))     ? JSON.parse(fs.readFileSync(this.PATH_USERS))     : []; }
+  _readFileExercises () { return (fs.existsSync(this.PATH_EXERCISES)) ? JSON.parse(fs.readFileSync(this.PATH_EXERCISES)) : []; }
+  getIndex (username) { // username or id
+    let ret;
+    ret = this.users.findIndex(item => item.username === username);
+    if (ret === -1) ret = this.users.findIndex(item => item._id === username);
+    return ret;
+  } 
+  getByIndex (index) { return this.user[index]; }
+  addUser (username) {
+    let id = this.users.length;
+    let newUser = { username: username, _id: id.toString() }
+    this.users.push(newUser);
+    
+    // update file - users
+    fs.writeFile(this.PATH_USERS, JSON.stringify(this.users), (err) => {
+        if (err) throw err;
+        console.log('users.json updated');
+    });
+    
+    return newUser;
+  }
+  addExercise (id, description, duration, date) {
+    let exercise = {_id: id, description: description, duration: duration, date: formatDate(date)}
+    this.exercises.push(exercise);
+    
+    // update file - users
+    fs.writeFile(this.PATH_EXERCISES, JSON.stringify(this.exercises), (err) => {
+        if (err) throw err;
+        console.log('exercises.json updated')
+    });    
+    
+    let index = this.getIndex(id);
+    let user = this.users[index];
+    return {username: user.username, _id: user._id, description: description, duration: duration, date: formatDate(date)};
+  }
+  getLog (id, from, to, limit) {
+    let index = this.getIndex(id);
+    let user = this.users[index];  
+    let log = this.exercises.map(item => {if (item._id === id) return item;});
+
+    // check if any filter parameters are set
+    if (from && checkIsDate(from)) log = log.filter(item => {if ((new Date(item.date)).getTime() >= from.getTime()) return item});
+    if (to && checkIsDate(to)) log = log.filter(item => {    if ((new Date(item.date)).getTime() <= to.getTime()) return item});
+    if (limit && !isNaN(limit)) log = log.slice(0, parseInt(limit));
+    
+    return {_id: user._id, username: user.username, count: log.length, log: log};
+  }
+}
+const user = new User();
+console.log(user)
+
 // add user
 app.route('/api/exercise/new-user').post((req, res) => {
   let name = req.body.username;
-  let index = getUserIndex(name);
+  let index = user.getIndex(name);
   
   if (index !== -1) return res.send('(Error) username already taken');
-  let id = users.length;
-  users.push({
-    username: name, 
-    _id: id.toString(), 
-  });
-  res.send(users[id]);
+  res.send(user.addUser(name));
 });
 
 // get users
 app.route('/api/exercise/users').get((req,res) => {
-  res.send(users);
+  res.send(user.users);
 });
 
 // add exercise
 app.route('/api/exercise/add').post((req,res) => {
   let id = req.body.userId;
-  let index = getUserIndex(id);
+  let index = user.getIndex(id);
   if (index === -1) return res.send('(Error) unknown userId');
   
   let info = req.body.description;
@@ -75,29 +124,23 @@ app.route('/api/exercise/add').post((req,res) => {
   let date = new Date(req.body.date);  
   date = (checkIsDate(date)) ? date : new Date(); 
   
-  exercises.push({_id: id, description: info, duration: time, date: formatDate(date)});
-  let user = users[index];
-  res.send({username: user.username, _id: user._id, description: info, duration: time, date: formatDate(date)});
+  res.send(user.addExercise(id, info, time, date));
 })
 
 // get exercise log
 app.route('/api/exercise/log').get((req,res) => {
   let id = req.query.userId;
-  let index = getUserIndex(id);  
+  let index = user.getIndex(id);  
   if (index === -1) return res.send('(Error) unknown userId');
   
-  let user = users[index];  
-  let log = exercises.map(item => {if (item._id === id) return item;});
-  
-  // check if any filter parameters are set
+  // filter parameters
   let from = new Date(req.query.from),
-      to = new Date (req.query.to),
-      limit = req.query.limit;
-  if (from && checkIsDate(from)) log = log.filter(item => {if ((new Date(item.date)).getTime() >= from.getTime()) return item});
-  if (to && checkIsDate(to)) log = log.filter(item => {    if ((new Date(item.date)).getTime() <= to.getTime()) return item});
-  if (limit && !isNaN(limit)) log = log.slice(0, parseInt(limit));
-  res.send({_id: user._id, username: user.username, count: log.length, log: log});
+    to = new Date (req.query.to),
+    limit = req.query.limit;
+  
+  res.send(user.getLog(id, from, to, limit));
 });
+
 
 
 // Not found middleware
