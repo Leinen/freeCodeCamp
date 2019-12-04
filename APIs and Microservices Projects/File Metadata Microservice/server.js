dd0aa6735cdd416630cd7712a076aa6f0fbfdae2
Leinen <36449763+Leinen@users.noreply.github.com>
2019-12-04 19:54:05 +0100

    Update server.js
    solved :)
    solution: https://fcc-as-file-metadata-microservice.glitch.me

--- APIs and Microservices Projects/File Metadata Microservice/server.js
+++ APIs and Microservices Projects/File Metadata Microservice/server.js
@@ -14,10 +14,37 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+
+
+// API here
+
+const multer  = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage });
+app.route('/api/fileanalyse').post(upload.single('upfile'), (req, res) => {
+  let name = req.file.originalname,
+      size = req.file.size,
+      type = req.file.mimetype;
+  res.send({"name": name,"type": type,"size": size});
+  console.log('file uploaded:')
+  console.log(req.file);
 });
+
+// check for errors
+app.post('/api/fileanalyse', function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.send('A Multer error occurred when uploading.')
+    } else if (err) {
+      res.send('An unknown error occurred when uploading.')
+    }
+ 
+    // Everything went fine.
+  })
+})
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
